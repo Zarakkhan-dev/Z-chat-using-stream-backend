1d7a6b5e9c54b5dcea1c9fd139d07144bf91979a
Zarak Khan <119649492+Zarakkhan-dev@users.noreply.github.com>
2025-05-29 19:56:32 +0500

    Fixes

--- index.js
+++ index.js
@@ -16,10 +16,26 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
-    origin: "https://z-chat-using-stream-frontend.vercel.app",
-    credentials: true
-  }));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+  "https://z-chat-using-stream-frontend.vercel.app/"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use("/api/v1/auth", AuthRoute);
 app.use("/api/v1/user", UserRoute);
