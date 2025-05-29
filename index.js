4e27ff8ac709f5b371e9eb0be4d852ce28eb1dc1
Zarak Khan <119649492+Zarakkhan-dev@users.noreply.github.com>
2025-05-29 21:41:10 +0500

    cors

--- index.js
+++ index.js
@@ -24,15 +24,8 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
