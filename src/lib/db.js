08ee4b1977552d56c2f39f67bfed801c684c48d1
Zarak Khan <119649492+Zarakkhan-dev@users.noreply.github.com>
2025-06-16 18:00:17 +0500

    testing

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const databaseConnection = async () => {
   try {
-    const connection = await mongoose.connect('mongodb+srv://zarakkhan23630:fpzc8MvA07XcWtXa@cluster0.clstlao.mongodb.net/chatapp?retryWrites=true&w=majority&appName=Cluster0');
+    const connection = await mongoose.connect(process.env.MONGO_URL);
     console.log(`Database Connected ${connection.connection.host}`);
   } catch (error) {
     console.log("Database connection Failed",error);
