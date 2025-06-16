dc75f52cdadfb32f46245f8b2d11abfe41b8eb8e
Zarak Khan <119649492+Zarakkhan-dev@users.noreply.github.com>
2025-06-16 17:24:23 +0500

    fixed

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,33 +4,39 @@ import { upsertStreamUser } from "../lib/stream.js";
 
 export const Login = async (req, res) => {
   try {
-    const {email, password} = req.body;
+    const { email, password } = req.body;
 
-    if(!email || !password) {
+    if (!email || !password) {
       return res.status(400).json({ message: "All fields are Required" });
     }
 
-    const verifyUser = await User.findOne({email});
-    if(!verifyUser) return res.status(401).json({message: "Invalid email or password"});
+    const verifyUser = await User.findOne({ email });
+    if (!verifyUser)
+      return res.status(401).json({ message: "Invalid email or password" });
 
-    const isPassowrd = await verifyUser.matchPassword(password)
-    if(!isPassowrd) return res.status(401).json({message: "Invalid email or password"});
-    
-    const token = jwt.sign({userId: verifyUser._id}, process.env.JWT_SECRET_KEY, {
-      expiresIn: "7d"
-    });
+    const isPassowrd = await verifyUser.matchPassword(password);
+    if (!isPassowrd)
+      return res.status(401).json({ message: "Invalid email or password" });
+
+    const token = jwt.sign(
+      { userId: verifyUser._id },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "7d",
+      }
+    );
 
     res.cookie("jwt", token, {
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      httpOnly: true, // prevent XSS attacks,
-      sameSite: "", // prevent CSRF attacks
-      secure: process.env.NODE_ENV === "production",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      httpOnly: true, // Helps mitigate XSS
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // Helps prevent CSRF
+      secure: process.env.NODE_ENV === "production", // Only send over HTTPS
     });
 
-    res.status(201).json({success: true, user: verifyUser});
+    res.status(201).json({ success: true, user: verifyUser });
   } catch (error) {
-    console.log("Sigin controller error", error)
-    res.status(500).json({message:"Internal Server Error", error: error})
+    console.log("Sigin controller error", error);
+    res.status(500).json({ message: "Internal Server Error", error: error });
   }
 };
 
@@ -69,7 +75,7 @@ export const Signup = async (req, res) => {
       password,
       profileImage: randomAvatar,
     });
-    
+
     try {
       await upsertStreamUser({
         id: newUser._id.toString(),
@@ -81,9 +87,13 @@ export const Signup = async (req, res) => {
       console.log("Error creating Stream user:", error);
     }
 
-    const token = jwt.sign({userId: newUser._id}, process.env.JWT_SECRET_KEY, {
-      expiresIn: "7d"
-    });
+    const token = jwt.sign(
+      { userId: newUser._id },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "7d",
+      }
+    );
 
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
@@ -91,52 +101,59 @@ export const Signup = async (req, res) => {
       sameSite: "none", // prevent CSRF attacks
       secure: true,
     });
-    
-    res.status(201).json({success: true, user: newUser});
+
+    res.status(201).json({ success: true, user: newUser });
   } catch (error) {
-    console.log("Signup controller error", error)
-    res.status(500).json({message:"Internal Server Error"})
+    console.log("Signup controller error", error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
 export const Logout = async (req, res) => {
   try {
     res.clearCookie("jwt");
-    res.status(200).json({message: "Logout successfully"})
+    res.status(200).json({ message: "Logout successfully" });
   } catch (error) {
-    console.log("Logout controller error", error)
-    res.status(500).json({message:"Internal Server Error"})
+    console.log("Logout controller error", error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
 export const onboard = async (req, res) => {
   try {
-   const userId = req.user._id;
-   const {fullName, bio, nativeLanguage, learningLanguage, location} = req.body;
-   if(!fullName || !bio || !nativeLanguage || !learningLanguage || !location) {
-    res.status(400).json({message:"All fields are required"});
-   }
-  
-   const updateUser = await User.findByIdAndUpdate (userId, {
-    ...req.body,
-    isOnboarded: true
-   })
+    const userId = req.user._id;
+    const { fullName, bio, nativeLanguage, learningLanguage, location } =
+      req.body;
+    if (
+      !fullName ||
+      !bio ||
+      !nativeLanguage ||
+      !learningLanguage ||
+      !location
+    ) {
+      res.status(400).json({ message: "All fields are required" });
+    }
 
-   try {
-    await upsertStreamUser({
-      id: updateUser._id.toString(),
-      name: updateUser.fullName,
-      image: updateUser.profilePic || "",
+    const updateUser = await User.findByIdAndUpdate(userId, {
+      ...req.body,
+      isOnboarded: true,
     });
-    console.log(`Stream user created for ${updateUser.fullName}`);
-  } catch (error) {
-    console.log("onboard controller error", error)
-    res.status(500).json({message:"Internal Server Error"})
-  }
 
-  res.status(200).json({success: true, user: updateUser});
+    try {
+      await upsertStreamUser({
+        id: updateUser._id.toString(),
+        name: updateUser.fullName,
+        image: updateUser.profilePic || "",
+      });
+      console.log(`Stream user created for ${updateUser.fullName}`);
+    } catch (error) {
+      console.log("onboard controller error", error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+
+    res.status(200).json({ success: true, user: updateUser });
   } catch (error) {
     console.log("onboard controller error", error);
-    res.status(500).json({message:"Internal Server Error"})
+    res.status(500).json({ message: "Internal Server Error" });
   }
-}
+};
